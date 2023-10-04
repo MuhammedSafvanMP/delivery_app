36ab794f995a635bc8726a2aeb537b1a0de808d5
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2023-10-04 18:21:06 +0530

    Dark-Light moode added

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "./images/sun.png";
+    }else{
+        icon.src = "./images/moon.png";
+    }
+}
