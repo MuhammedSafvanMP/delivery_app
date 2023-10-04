2e13d6f3af6fe57639564abd0038354c9086daed
Muhammed Safvan MP <muhammedsafvanmp7@gmail.com>
2023-10-04 20:10:29 +0530

    Toggle-navbar design

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
+// Nav bar toggle code
+
+const toggleShow = document.querySelector(".show");
+const menu = document.querySelector(".hide");
+
+toggleShow.addEventListener("click", function () {
+  menu.classList.toggle("show");
+});
+
+// Dark & Light theme code
+
 var icon = document.getElementById("icon");
 
-icon.onclick = function() {
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "./images/sun.png";
-    }else{
-        icon.src = "./images/moon.png";
-    }
-}
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "./images/sun.png";
+  } else {
+    icon.src = "./images/moon.png";
+  }
+};
